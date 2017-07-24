b07f3310c80d8e0a89fa61db291fc7cb4f9e35fd
Maxim Suntsev <masuntsev@edu.hse.ru>
2017-07-25 03:21:59 +0500

    Принято.

--- Practice/Task2/Matrix.h
+++ Practice/Task2/Matrix.h
@@ -1,238 +1,343 @@
 #pragma once
 
-#include <string>
-#include <sstream>
+#include <algorithm>
+#include <initializer_list>
+#include <type_traits>
 #include <utility>
+#include <string>
 #include <vector>
 
 #include <assert.h>
 
 template <typename T>
-class MatrixRow
+class Matrix;
+
+template <typename T>
+class MatrixRow;
+
+template <typename T>
+Matrix<T> operator*(const Matrix<T>& lhs, const Matrix<T>& rhs)
 {
-public:
-    MatrixRow() : m_columns(nullptr), m_width(0) {}
-    MatrixRow(std::size_t width);
-    MatrixRow(std::size_t width, T const* data);
-    ~MatrixRow();
-    MatrixRow(const MatrixRow<T> & rhs);
-    MatrixRow<T>& operator=(const MatrixRow<T>& rhs);
-    MatrixRow(MatrixRow<T> && rhs);
-    MatrixRow<T>& operator=(MatrixRow<T>&& rhs);
+    assert(lhs.m_height == rhs.m_width);
+    auto size = lhs.m_height;
+    Matrix<T> result = Matrix<T>(rhs.m_width, lhs.m_height);
 
-    T& operator[](std::size_t index);
-    T& operator[](std::size_t index) const;
+    for (int i = 0; i < lhs.m_height; ++i)
+        for (int j = 0; j < rhs.m_width; ++j)
+        {
+            T sum = T();
+            for (int k = 0; k < size; ++k)
+            {
+                sum += lhs[i][k] * rhs[k][j];
+            }
 
-private:
-    T* m_columns = nullptr;
-    std::size_t m_width;
-};
+            result[i][j] = sum;
+        }
+
+    return std::move(result);
+}
+
+template <typename T>
+Matrix<T> operator%(const Matrix<T>& lhs, T divider)
+{
+    Matrix<T> result = Matrix<T>(lhs);
+
+    for (int i = 0; i < lhs.m_height; ++i)
+        for (int j = 0; j < lhs.m_width; ++j)
+            result[i][j] %= divider;
+
+    return std::move(result);
+}
 
 template <typename T>
 class Matrix
 {
 public:
-    Matrix(std::size_t width, std::size_t height);
-    Matrix(std::size_t width, std::size_t height, T const* data);
+    Matrix(int width, int height);
+    Matrix(int width, int height, T init[]);
+    Matrix(int width, int height, std::vector<T>& init);
+    Matrix(int width, int height, std::initializer_list<T> init);
+    Matrix(int width, int height, MatrixRow<T> init[]);
     ~Matrix();
     Matrix(const Matrix<T>& rhs);
     Matrix<T>& operator=(const Matrix<T>& rhs);
     Matrix(Matrix<T>&& rhs);
     Matrix<T>& operator=(Matrix<T>&& rhs);
 
-    MatrixRow<T>& operator[](std::size_t index);
-    MatrixRow<T>& operator[](std::size_t index) const;
-    Matrix<T> operator*=(const Matrix<T>& rhs);
+    const MatrixRow<T>& operator[](int index) const;
+    MatrixRow<T>& operator[](int index);
+    const T& operator()(int i, int j) const;
+    T& operator()(int i, int j);
+
+    std::string toString() const;
+
+    friend Matrix<T> operator*<>(const Matrix<T>& lhs, const Matrix<T>& rhs);
+    friend Matrix<T> operator%<>(const Matrix<T>& lhs, T divider);
 
 private:
-    Matrix(std::size_t width, std::size_t height, MatrixRow<T> const* data);
-
+    int m_width;
+    int m_height;
     MatrixRow<T>* m_rows = nullptr;
-    std::size_t m_width;
-    std::size_t m_height;
+};
+
+// using T = int;
+template <typename T>
+class MatrixRow
+{
+public:
+    MatrixRow() {};
+    MatrixRow(int width);
+    MatrixRow(int width, T init[]);
+    MatrixRow(int width, std::vector<T>& init);
+    ~MatrixRow();
+    MatrixRow(const MatrixRow<T>& rhs);
+    MatrixRow<T>& operator=(const MatrixRow<T>& rhs);
+    MatrixRow(MatrixRow<T>&& rhs);
+    MatrixRow<T>& operator=(MatrixRow<T>&& rhs);
+
+    const T& operator[](int index) const;
+    T& operator[](int index);
+
+private:
+    void internalInit(T init[]);
+
+    int m_width;
+    T* m_data = nullptr;
 };
 
 template<typename T>
-inline Matrix<T>::Matrix(std::size_t width, std::size_t height)
+inline MatrixRow<T>::MatrixRow(int width) :
+    m_width(width), m_data(new T[width])
 {
-    assert(width > 0 && height > 0);
-
-    m_width = width;
-    m_height = height;
-
-    m_rows = new MatrixRow<T>[m_height];
-
-    for (std::size_t i = 0; i < m_height; ++i)
-        m_rows[i] = MatrixRow<T>(m_width);
+    assert(m_width > 0);
 }
 
 template<typename T>
-inline Matrix<T>::Matrix(std::size_t width, std::size_t height, T const* data) :
-    Matrix<T>(width, height)
+inline MatrixRow<T>::MatrixRow(int width, T init[]) :
+    MatrixRow(width)
 {
-    // assert(data.size() == m_width * m_height);
-
-    for (std::size_t i = 0; i < m_height; ++i)
-        for (std::size_t j = 0; j < m_width; ++j)
-            m_rows[i][j] = data[j + i * m_width];
+    assert(m_width > 0);
+    m_data = new T[width];
+    auto p = init;
+    std::advance(p, width);
+    std::copy(init, p, m_data);
+    // internalInit(init.data());
 }
 
 template<typename T>
-inline Matrix<T>::~Matrix()
+inline MatrixRow<T>::MatrixRow(int width, std::vector<T>& init) :
+    MatrixRow(width, init.data())
 {
-    delete[] m_rows;
-    m_rows = nullptr;
-}
-
-template<typename T>
-inline Matrix<T>::Matrix(const Matrix<T>& rhs)
-{
-    this->~Matrix<T>();
-    *this = Matrix<T>(rhs.m_width, rhs.m_height, rhs.m_rows);
-}
-
-template<typename T>
-inline Matrix<T>& Matrix<T>::operator=(const Matrix<T>& rhs)
-{
-    this->~Matrix<T>();
-    return *this = Matrix<T>(rhs.m_width, rhs.m_height, rhs.m_rows);
-}
-
-template<typename T>
-inline Matrix<T>::Matrix(Matrix<T>&& rhs)
-{
-    this->~Matrix<T>();
-    m_width = rhs.m_width;
-    m_height = rhs.m_height;
-    m_rows = rhs.m_rows;
-    rhs.m_rows = nullptr;
-}
-
-template<typename T>
-inline Matrix<T>& Matrix<T>::operator=(Matrix<T>&& rhs)
-{
-    this->~Matrix<T>();
-    m_width = rhs.m_width;
-    m_height = rhs.m_height;
-    m_rows = rhs.m_rows;
-    rhs.m_rows = nullptr;
-    return *this;
-}
-
-template<typename T>
-inline MatrixRow<T>& Matrix<T>::operator[](std::size_t index)
-{
-    assert(index >= 0 && index < m_width);
-
-    return m_rows[index];
-}
-
-template<typename T>
-inline MatrixRow<T>& Matrix<T>::operator[](std::size_t index) const
-{
-    assert(index >= 0 && index < m_width);
-
-    return m_rows[index];
-}
-
-template<typename T>
-inline Matrix<T> Matrix<T>::operator*=(const Matrix<T>& rhs)
-{
-    assert(m_width == rhs.m_height);
-
-    Matrix<T> result = Matrix<T>(m_width, rhs.m_height);
-    auto size = m_width;
-
-    for (std::size_t i = 0; i < rhs.m_height; ++i)
-        for (std::size_t j = 0; j < m_width; ++j)
-        {
-            T sum = T();
-            for (std::size_t k = 0; k < size; ++k)
-                sum += (*this)[i][k] * rhs[k][j];
-            result[i][j] = sum;
-        }
-
-    return result;
-}
-
-template<typename T>
-inline Matrix<T>::Matrix(std::size_t width, std::size_t height, MatrixRow<T> const* data) :
-    Matrix<T>(width, height)
-{
-    for (std::size_t i = 0; i < m_height; ++i)
-        for (std::size_t j = 0; j < m_width; ++j)
-            m_rows[i][j] = data[i][j];
-}
-
-template<typename T>
-inline MatrixRow<T>::MatrixRow(std::size_t width)
-{
-    assert(width > 0);
-
-    m_width = width;
-    m_columns = new T[m_width];
-}
-
-template<typename T>
-inline MatrixRow<T>::MatrixRow(std::size_t width, T const* data) :
-    MatrixRow<T>(width)
-{
-    for (std::size_t i = 0; i < m_width; ++i)
-        m_columns = data[i];
+    // internalInit(init.data());
 }
 
 template<typename T>
 inline MatrixRow<T>::~MatrixRow()
 {
-    delete[] m_columns;
-    m_columns = nullptr;
+    // if (m_data != nullptr)
+    delete[] m_data;
 }
 
 template<typename T>
 inline MatrixRow<T>::MatrixRow(const MatrixRow<T>& rhs)
 {
-    this->~MatrixRow<T>();
-    *this = MatrixRow<T>(rhs.m_width, rhs.m_columns);
+    this->operator=(rhs);
 }
 
 template<typename T>
 inline MatrixRow<T>& MatrixRow<T>::operator=(const MatrixRow<T>& rhs)
 {
     this->~MatrixRow<T>();
-    return *this = MatrixRow<T>(rhs.m_width, rhs.m_columns);
+
+    return *this = MatrixRow<T>(rhs.m_width, rhs._mdata);
 }
 
 template<typename T>
 inline MatrixRow<T>::MatrixRow(MatrixRow<T>&& rhs)
 {
-    this->~MatrixRow<T>();
-    m_width = rhs.m_width;
-    m_columns = rhs.m_columns;
-    rhs.m_columns = nullptr;
+    this->operator=(rhs);
 }
 
 template<typename T>
 inline MatrixRow<T>& MatrixRow<T>::operator=(MatrixRow<T>&& rhs)
 {
     this->~MatrixRow<T>();
+
     m_width = rhs.m_width;
-    m_columns = rhs.m_columns;
-    rhs.m_columns = nullptr;
+    m_data = rhs.m_data;
+    rhs.m_data = nullptr;
+
     return *this;
 }
 
 template<typename T>
-inline T& MatrixRow<T>::operator[](std::size_t index)
+inline const T & MatrixRow<T>::operator[](int index) const
 {
     assert(index >= 0 && index < m_width);
-
-    return m_columns[index];
+    return m_data[index];
 }
 
 template<typename T>
-inline T& MatrixRow<T>::operator[](std::size_t index) const
+inline T & MatrixRow<T>::operator[](int index)
 {
     assert(index >= 0 && index < m_width);
+    return m_data[index];
+}
 
-    return m_columns[index];
+template<typename T>
+inline void MatrixRow<T>::internalInit(T init[])
+{
+    assert(m_width > 0);
+    auto p = init;
+    std::advance(p, m_width);
+    std::copy(init, p, m_data);
+}
+
+template<typename T>
+inline Matrix<T>::Matrix(int width, int height) :
+    m_width(width), m_height(height)
+{
+    assert(width > 0 && height > 0);
+    m_rows = new MatrixRow<T>[m_height];
+
+    for (int i = 0; i < m_height; ++i)
+        m_rows[i] = std::move(MatrixRow<T>(m_width));
+}
+
+template<typename T>
+inline Matrix<T>::Matrix(int width, int height, T init[]) :
+    m_width(width), m_height(height)
+{
+    assert(width > 0 && height > 0);
+    m_rows = new MatrixRow<T>[m_height];
+
+    for (int i = 0; i < m_height; ++i)
+    {
+        std::advance(init, i * m_width);
+        m_rows[i] = MatrixRow<T>(m_width, init);
+    }
+}
+
+template<typename T>
+inline Matrix<T>::Matrix(int width, int height, std::vector<T>& init) :
+    Matrix(width, height, init.data())
+{
+}
+
+template<typename T>
+inline Matrix<T>::Matrix(int width, int height, std::initializer_list<T> init) :
+    Matrix<T>(width, height)
+{
+    int i = 0, j = 0;
+
+    for (auto a : init)
+    {
+        m_rows[i][j++] = a;
+
+        if (j >= m_width)
+            j = 0, ++i;
+    }
+}
+
+template<typename T>
+inline Matrix<T>::Matrix(int width, int height, MatrixRow<T> init[]) :
+    Matrix<T>(width, height)
+{
+    for (int i = 0; i < m_height; ++i)
+        for (int j = 0; j < m_width; ++j)
+            m_rows[i][j] = init[i][j];
+}
+
+template<typename T>
+inline Matrix<T>::~Matrix()
+{
+    // for (int i = 0; i < m_height; ++i)
+    //	m_rows[i].~MatrixRow<T>();
+
+    delete[] m_rows;
+}
+
+template<typename T>
+inline Matrix<T>::Matrix(const Matrix<T>& rhs)
+{
+    this->operator=(rhs);
+}
+
+template<typename T>
+inline Matrix<T>& Matrix<T>::operator=(const Matrix<T>& rhs)
+{
+    this->~Matrix<T>();
+
+    return *this = Matrix<T>(rhs.m_width, rhs.m_height, rhs.m_rows);
+}
+
+template<typename T>
+inline Matrix<T>::Matrix(Matrix<T>&& rhs)
+{
+    this->operator=(rhs);
+}
+
+template<typename T>
+inline Matrix<T>& Matrix<T>::operator=(Matrix<T>&& rhs)
+{
+    this->~Matrix<T>();
+
+    m_width = rhs.m_width;
+    m_height = rhs.m_height;
+    m_rows = rhs.m_rows;
+
+    rhs.m_rows = nullptr;
+    return *this;
+}
+
+template<typename T>
+inline const MatrixRow<T>& Matrix<T>::operator[](int index) const
+{
+    assert(index >= 0 && index < m_height);
+    return m_rows[index];
+}
+
+template<typename T>
+inline MatrixRow<T>& Matrix<T>::operator[](int index)
+{
+    assert(index >= 0 && index < m_height);
+    return m_rows[index];
+}
+
+template<typename T>
+inline const T & Matrix<T>::operator()(int i, int j) const
+{
+    assert(i >= 0 && i < m_height);
+    return m_rows[i][j];
+}
+
+template<typename T>
+inline T & Matrix<T>::operator()(int i, int j)
+{
+    assert(i >= 0 && i < m_height);
+    return m_rows[i][j];
+}
+
+template<typename T>
+inline std::string Matrix<T>::toString() const
+{
+    auto result = std::string("{");
+
+    for (int i = 0; i < m_height; ++i)
+    {
+        result.append("\n\t");
+
+        for (int j = 0; j < m_width; ++j)
+        {
+            result.append(std::to_string((*this)[i][j]));
+            result.append(", ");
+        }
+
+        result.pop_back();
+        result.pop_back();
+    }
+
+    result.append("\n};\n\n");
+
+    return std::move(result);
 }
--- Practice/Task2/Program.cpp
+++ Practice/Task2/Program.cpp
@@ -1,11 +1,11 @@
-#include <algorithm>
+/*#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <vector>
 
 #include "Matrix.h"
 
-#define HOME
+// #define HOME
 
 int main(int argc, char** argv)
 {
@@ -17,8 +17,8 @@ int main(int argc, char** argv)
 #define ins std::cin
 #define outs std::cout
 #else
-	auto ins = std::ifstream("input.txt");
-	auto outs = std::ofstream("output.txt");
+    auto ins = std::ifstream("input.txt");
+    auto outs = std::ofstream("output.txt");
 #endif // HOME
 
 	int matrixCount, size;
@@ -36,16 +36,104 @@ int main(int argc, char** argv)
 			data.push_back(cell);
 		}
 
-		matrices.push_back(IntMatrix(size, size, data.data()));
+		matrices.push_back(IntMatrix(size, size, data));
 		data.clear();
 	}
 
-	IntMatrix result = std::move(matrices[0]);
+	IntMatrix result = matrices[0];
 
-	std::for_each(++matrices.cbegin(), matrices.cend(), [&result](auto& m)
-	{
-		result *= m;
-	});
+    if (matrices.size() > 1)
+    {
+        std::for_each(++matrices.begin(), matrices.end(), [&result, modulo](const IntMatrix& m)
+        {
+            result = result * m;
+            result = result % modulo;
+        });
+    }
 
-	outs << result[i][j];
+	outs << result[--i][--j] % modulo;
+}*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MATRIX_SIZE 200
+#define MATRIX_COUNT 200
+
+typedef int *MATRIX, **PMATRIX;
+
+MATRIX makematrix(int size)
+{
+    return (MATRIX)malloc(sizeof(int) * MATRIX_SIZE * MATRIX_SIZE);
+}
+
+MATRIX readmatrix(MATRIX dst, int size)
+{
+    int i;
+
+    for (i = 0; i < size * size; ++i)
+        scanf("%d", &dst[i]);
+
+    return dst;
+}
+
+MATRIX mulmodij(MATRIX dst, MATRIX src, int size, int modulo, int row, int col)
+{
+    int i, j, k;
+    int sum;
+    static MATRIX result = makematrix(size);
+
+    i = row;
+    const int ioff = i * size;
+    // j = col;
+    // for (i = 0; i < size; ++i)
+    for (j = 0; j < size; ++j)
+    {
+        sum = 0;
+        for (k = 0; k < size; ++k)
+        {
+            sum += dst[ioff + k] * src[k * size + j];
+        }
+
+        result[ioff + j] = sum;
+    }
+
+    // for (i = 0; i < size; ++i)
+    for (j = 0; j < size; ++j)
+    {
+        int index = i * size + j;
+        dst[index] = result[index] % modulo;
+    }
+
+    return dst;
+}
+
+int main()
+{
+    MATRIX first, next;
+    int matrixCount, size;
+    int i, j;
+    int modulo;
+    int x;
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    scanf("%d %d %d %d %d", &matrixCount, &size, &i, &j, &modulo);
+    --i, --j;
+
+    first = makematrix(size);
+    next = makematrix(size);
+    readmatrix(first, size);
+
+    for (x = 1; x < matrixCount; ++x)
+    {
+        readmatrix(next, size);
+        mulmodij(first, next, size, modulo, i, j);
+    }
+
+    printf("%d", first[i * size + j] % modulo);
+
+    free(first);
+    free(next);
 }
