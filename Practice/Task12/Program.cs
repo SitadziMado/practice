cd958118fb32ee7c8c3d0dd672c71ab372d4212a
Maxim Suntsev <masuntsev@edu.hse.ru>
2017-08-29 03:07:13 +0500

    Задание 12 сделано.

---
+++ Practice/Task12/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    class Program
+    {
+        private const int Multiplier = 32;
+
+        private const int RawMinValue = -1024;
+        private const int RawMaxValue = -RawMinValue;
+
+        private const int MinValue = RawMinValue * Multiplier;
+        private const int MaxValue = -MinValue;
+
+        private static IEnumerable<int> mRange = from v in Enumerable.Range(RawMinValue, RawMaxValue) where true select v * Multiplier;
+        private static int[] mAscending = mRange.ToArray();
+        private static int[] mDescending = mRange.Reverse().ToArray();
+        private static int[] mScattered = mRange.ToArray();
+
+        static void Main(string[] args)
+        {
+            SortAndPrintStatistics();
+            Console.ReadKey();
+        }
+
+        private static int[] Shuffle(int[] array)
+        {
+            Random rnd = new Random();
+            return array.OrderBy(x => rnd.Next()).ToArray();
+        }
+
+        private static void SortAndPrintStatistics()
+        {
+
+            Console.WriteLine("Были созданы и отсортированы непрерывные целочисленные диапазоны от {0} то {1}.", MinValue, MaxValue);
+
+            SortAndPrintTrees();
+            SortAndPrintArrays();
+        }
+
+        private static void SortAndPrintTrees()
+        {
+            mScattered = Shuffle(mScattered);
+
+            var ascendingTree = new BinaryTree<int>(mAscending);
+            var descendingTree = new BinaryTree<int>(mDescending);
+            var scatteredTree = new BinaryTree<int>(mScattered);
+
+            Console.WriteLine(
+                "Количество сравнений, доступов к памяти, рекурсивных спусков для метода сортировки с помощью двоичного дерева: "
+            );
+            Console.WriteLine(
+                "\tВозрастающий список: {0}, {1}, {2}.",
+                ascendingTree.ComparisonCount,
+                ascendingTree.MemoryAccessCount,
+                ascendingTree.DfsEntryCount
+            );
+            Console.WriteLine(
+                "\tУбывающий список: {0}, {1}, {2}.",
+                descendingTree.ComparisonCount,
+                descendingTree.MemoryAccessCount,
+                descendingTree.DfsEntryCount
+            );
+            Console.WriteLine(
+                "\tНеупорядоченный список: {0}, {1}, {2}.",
+                scatteredTree.ComparisonCount,
+                scatteredTree.MemoryAccessCount,
+                scatteredTree.DfsEntryCount
+            );
+
+            Console.WriteLine();
+        }
+
+        private static void SortAndPrintArrays()
+        {
+            var ascendingArray = mAscending.CountingSort(MinValue, MaxValue);
+            var ascendingStat = Utility.LastMemoryAccessCount;
+            var descendingArray = mDescending.CountingSort(MinValue, MaxValue);
+            var descendingStat = Utility.LastMemoryAccessCount;
+            var scatteredArray = mScattered.CountingSort(MinValue, MaxValue);
+            var scatteredStat = Utility.LastMemoryAccessCount;
+            
+            Console.WriteLine(
+                "Количество доступов к памяти для метода сортировки подсчетом: "
+            );
+            
+            Console.WriteLine("\tВозрастающий список: {0}.", ascendingStat);
+            Console.WriteLine("\tУбывающий список: {0}.", descendingStat);
+            Console.WriteLine("\tНеупорядоченный список: {0}.", scatteredStat);
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Practice/Task12/Utility.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    public static class Utility
+    {
+        public static IEnumerable<int> CountingSort(this IEnumerable<int> range, int minValue, int maxValue)
+        {
+            try
+            {
+                if (minValue > maxValue)
+                    throw new ArgumentOutOfRangeException("Минимальное значение не должно превышать максимальное.");
+
+                // if (maxValue - minValue > CountingSortLimit)
+                //     throw new ArgumentOutOfRangeException("Слишком большой диапазон чисел.");
+
+                int size = range.Count();
+                int[] result = new int[size];
+                int[] count = new int[maxValue - minValue + 1];
+
+                LastMemoryAccessCount = 0;
+
+                foreach (var v in range)
+                {
+                    ++count[v - minValue];
+                    ++LastMemoryAccessCount;
+                }
+
+                int index = 0;
+
+                for (int i = minValue; i <= maxValue; ++i)
+                    for (int j = count[i - minValue]; j-- != 0;)
+                    {
+                        result[index++] = i;
+                        ++LastMemoryAccessCount;
+                    }
+
+                return result.AsEnumerable();
+            }
+            catch (OutOfMemoryException e)
+            {
+                throw new OutOfMemoryException("Слишком большой диапазон чисел.", e);
+            }
+        }
+
+        public const int CountingSortLimit = 1000000;
+
+        public static int LastMemoryAccessCount { get; set; }
+    }
+}
